17637435d7dcff385fce069d9865a9907c2a8674
Sumit jangir <154914060+Sumit-Jangir@users.noreply.github.com>
2024-03-10 21:53:33 +0530

    Add files via upload

---
+++ 01project/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(5);
+
+  const AddValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const SubValue = () => {
+    if (counter > 0) setCounter(counter - 1);
+  };
+
+  return (
+    <>
+      <h1> Add And Subtract Value</h1>
+      <h2>counter value: {counter}</h2>
+      <button onClick={AddValue}> Add Value</button> <br />
+      <br />
+      <button onClick={SubValue}>Subtract Value</button>
+    </>
+  );
+}
+
+export default App;
